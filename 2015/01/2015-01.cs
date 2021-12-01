7ed5c02c15b733a8eaf6da8f39ac0e63749d8817
Cesar Garcia <cgarcia@bungie.com>
2021-11-30 17:18:38 -0800

    Initial commit.

---
+++ 2015/01/2015-01.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+// Puzzle: https://adventofcode.com/2015/day/1
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@".\input.txt");
+            int floor = 0;
+            for(int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if ('(' == c)
+                {
+                    floor ++;
+                }
+                else if (')' == c)
+                {
+                    floor--;
+                }
+            }
+            Console.WriteLine(floor);
+        }
+    }
+}
