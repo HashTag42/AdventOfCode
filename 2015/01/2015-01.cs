f221ab73af74890972c540b3b3f596bfd7bb86a7
Cesar Garcia <cgarcia@bungie.com>
2021-11-30 19:03:10 -0800

    Solved part 2.

--- 2015/01/2015-01.cs
+++ 2015/01/2015-01.cs
@@ -3,27 +3,38 @@ using System.IO;
 
 // Puzzle: https://adventofcode.com/2015/day/1
 
-// --- Day 1: Not Quite Lisp ---
-// Santa was hoping for a white Christmas, but his weather machine's "snow" function is powered by stars, and he's fresh out! To save Christmas, he needs you to collect fifty stars by December 25th.
+/* --- Day 1: Not Quite Lisp ---
+    Santa was hoping for a white Christmas, but his weather machine's "snow" function is powered by stars,
+    and he's fresh out! To save Christmas, he needs you to collect fifty stars by December 25th.
 
-// Collect stars by helping Santa solve puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+    Collect stars by helping Santa solve puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
 
-// Here's an easy puzzle to warm you up.
+    Here's an easy puzzle to warm you up.
 
-// Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - the directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the instructions one character at a time.
+    Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - the directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the instructions one character at a time.
 
-// An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
+    An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
 
-// The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
+    The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
 
-// For example:
+    For example:
 
-// (()) and ()() both result in floor 0.
-// ((( and (()(()( both result in floor 3.
-// ))((((( also results in floor 3.
-// ()) and ))( both result in floor -1 (the first basement level).
-// ))) and )())()) both result in floor -3.
-// To what floor do the instructions take Santa?
+    (()) and ()() both result in floor 0.
+    ((( and (()(()( both result in floor 3.
+    ))((((( also results in floor 3.
+    ()) and ))( both result in floor -1 (the first basement level).
+    ))) and )())()) both result in floor -3.
+    To what floor do the instructions take Santa?
+
+    --- Part Two ---
+    Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
+
+    For example:
+
+    ) causes him to enter the basement at character position 1.
+    ()()) causes him to enter the basement at character position 5.
+    What is the position of the character that causes Santa to first enter the basement?
+*/
 
 namespace _01
 {
@@ -31,21 +42,34 @@ namespace _01
     {
         static void Main(string[] args)
         {
-            string text = File.ReadAllText(@".\input.txt");
-            int floor = 0;
+            string filePath;
+            // filePath = @".\testInput1.txt";
+            // filePath = @".\testInput5.txt";
+            filePath = @".\Input.txt";
+            string text = File.ReadAllText(filePath);
+            int finalFloor = 0;
+            int basementChar = 0;
+            bool basementFound = false;
             for(int i = 0; i < text.Length; i++)
             {
                 char c = text[i];
-                if ('(' == c)
+                if ( '(' == c )
                 {
-                    floor++;
+                    finalFloor++;
                 }
-                else if (')' == c)
+                else if ( ')' == c )
                 {
-                    floor--;
+                    finalFloor--;
+                }
+                if( (!basementFound) && (-1 == finalFloor) )
+                {
+                    basementChar = i+1;
+                    basementFound = true;
                 }
             }
-            Console.WriteLine(floor);
+            Console.WriteLine("Final floor: {0}", finalFloor);
+
+            Console.WriteLine("Santa entered the basement at character {0}", basementChar);
         }
     }
 }
