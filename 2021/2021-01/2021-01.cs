16f0fead82853c0d5e5c084689456f08679fed50
Cesar Garcia <cgarcia@bungie.com>
2021-11-30 23:59:35 -0800

    Solved 2021-01 Part 1

---
+++ 2021/2021-01/2021-01.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+// Puzzle: https://adventofcode.com/2021/day/1
+
+
+
+namespace _2021_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(processFile(@".\inputTest.txt"));
+            Console.WriteLine(processFile(@".\input.txt"));
+        }
+
+        private static string processFile(string FilePath)
+        {
+            uint result = processLines(FilePath);
+            string theString = "Processing " + FilePath + " // ";
+            theString += "Increased count: " + result;
+
+            return theString;
+        }
+
+        private static uint processLines(string FilePath)
+        {
+            uint increasedCount = 0;
+            uint previous = 0;
+            uint current = 0;
+            uint lineCount = 1;
+
+            foreach(string line in File.ReadLines(FilePath))
+            {
+                previous = current;
+                current  = uint.Parse(line);
+                if (lineCount > 1)
+                {
+                    if (current > previous)
+                    {
+                        increasedCount++;
+                    }
+                }
+                lineCount++;
+            }
+
+            return increasedCount;
+        }
+
+    }
+}
