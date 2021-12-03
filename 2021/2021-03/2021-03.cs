c6ec96aa37d85f9b4cc3f910ad833115ab34d0a2
HashTag42 <ceegee@outlook.com>
2021-12-03 00:08:34 -0800

    Improved partial solution.

--- 2021/2021-03/2021-03.cs
+++ 2021/2021-03/2021-03.cs
@@ -9,59 +9,48 @@ namespace _2021_03
         {
             string FilePath = @".\inputTest.txt";
             // string FilePath = @".\input.txt";
-            // input = File.ReadAllText(FilePath);
 
             Console.WriteLine(solve_2021_03_Part1(FilePath));
         }
 
         private static int solve_2021_03_Part1(string FilePath) {
-            int[,] matrix = new int[5,12];
-            int lineCount = 0;
-            int charCount = 0;
-            foreach(string line in File.ReadLines(FilePath)) {
-                charCount = 0;
-                foreach(char c in line) {
-                    matrix[charCount,lineCount] = int.Parse(c.ToString());
-                    charCount++;
-                }
-                lineCount++;
-            }
-            // printMatrix(matrix);
+            const int rowLength = 5;
 
-            int[,] summary = new int[matrix.GetLength(0),4];
-            int maxX = matrix.GetLength(0);
-            int maxY = matrix.GetLength(1);
-            for(int y = 0; y < maxY; y++) {
-                for(int x = 0; x < maxX; x++) {
-                    if(0 == matrix[x,y]) {
+            int[,] summary = new int[rowLength,4];
+
+            foreach(string line in File.ReadLines(FilePath)) {
+                int x = 0;
+                foreach(char c in line) {
+                    if('0' == c) {
                         summary[x,0]++;
                     } else {
                         summary[x,1]++;
                     }
+                    x++;
                 }
             }
-            for(int x = 0; x < maxX; x++) {
-                int countOfZeros = summary[x,0];
-                int countOfOnes  = summary[x,1];
-                if(countOfZeros > countOfOnes) {
+
+            for(int x = 0; x < rowLength; x++) {
+                if(summary[x,0] > summary[x,1]) {
                     summary[x,2] = 0;
                     summary[x,3] = 1;
-
                 } else {
                     summary[x,2] = 1;
                     summary[x,3] = 0;
                 }
             }
-            // printMatrix(summary);
 
-            BinaryString gammaRate = new BinaryString("10110");
-            BinaryString epsilonRate = new BinaryString("01001");
+            printMatrix(summary);
+
+            string gammaRateString = "10110";
+            string epsilonRateString = "01001";
+
+            BinaryString gammaRate = new BinaryString(gammaRateString);
+            BinaryString epsilonRate = new BinaryString(epsilonRateString);
 
             int powerConsumption = gammaRate.ToInt() * epsilonRate.ToInt();
-
             return powerConsumption;
         }
-
 
         private static void printMatrix(int[,] matrix) {
             int maxX = matrix.GetLength(0);
