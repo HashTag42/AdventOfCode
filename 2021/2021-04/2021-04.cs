750e668fa5da01d1c16dba175dfc535554d1d44d
Cesar Garcia <cgarcia@bungie.com>
2021-12-04 02:57:31 -0800

    Reading boards from the input file.

--- 2021/2021-04/2021-04.cs
+++ 2021/2021-04/2021-04.cs
@@ -1,29 +1,54 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Diagnostics;
 
-namespace _2021_04
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace _2021_04 {
+    class Program {
+        static void Main(string[] args) {
+            // The expected results are <PART1> and <PART2>
+            Console.WriteLine(SolvePuzzleUsingFile(@".\inputTest.txt"));
+            // The expected results are <PART1> and <PART2>
+            // Console.WriteLine(SolvePuzzleUsingFile(@".\input.txt"));
+        }
+
+        private static string SolvePuzzleUsingFile(string FilePath) {
             List<string> input = new List<string>();
+            foreach(string line in File.ReadAllLines(FilePath)) { input.Add(line); }
+            string result = null;
+            result  = "Solving puzzle using file: " + FilePath;
+            result += "\n\t Answer to Part 1: " + solvePart1(input);
+            result += "\n\t Answer to Part 2: " + solvePart2(input);
+            return result;
+        }
 
-            string inputFile = null;
-            inputFile = @"inputTest.txt";
-            // inputFile = @"input.txt";
-
-            foreach(string line in File.ReadAllLines(inputFile)) {
-                input.Add(line);
+        private static int solvePart1(List<string> input) {
+            List<BingoBoard> boards = new List<BingoBoard>();
+            for(int i=2 ; i<input.Count; i+=6) {
+                int firstLineIndex = i;
+                int lineCount = 5;
+                List<string> boardInput = input.GetRange(firstLineIndex, lineCount);
+                BingoBoard newBoard = new BingoBoard(boardInput);
+                boards.Add(newBoard);
+                Console.WriteLine(newBoard);
             }
 
-            Console.WriteLine(solvePart1(input));
+            // TODO: Draw numbers to determine the winning board
+
+            // TODO: Get the SumUnmarkedCells value from the winning board
+            int sumUnmarkedCells = boards[2].SumUnmarkedCells();
+
+            // TODO: Use the last drawn number
+            int winningNumber = 24;
+
+            int score = sumUnmarkedCells * winningNumber;
+            return score;
         }
-        private static int solvePart1(List<string> input) {
+
+        private static int solvePart2(List<string> input) {
             int answer = input.Count;
-            BingoBoard board = new BingoBoard();
             return answer;
         }
+
     }
 }
--- 2021/2021-04/BingoBoard.cs
+++ 2021/2021-04/BingoBoard.cs
@@ -1,26 +1,79 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 public class BingoBoard {
 
-    public BingoCell[,] board { get; }
+    public BingoCell[,] Board = new BingoCell[5,5];
 
-    public BingoBoard()
-    {
-        board = new BingoCell[5,5];
+    public bool IsBingo { get; }
+
+    public int SumUnmarkedCells() {
+        int sum = 0;
+        for(int row = 0; row < 5; row++) {
+            for(int col = 0; col <5; col++) {
+                if(!Board[row,col].IsMarked) {
+                    sum += Board[row,col].Number;
+                }
+            }
+        }
+        return sum;
     }
 
+    public override string ToString()
+    {   string result = null;
+        for(int row = 0; row < 5; row++) {
+            for(int col = 0; col < 5; col++) {
+                result += Board[row,col].Number.ToString("D2");
+                if(Board[row,col].IsMarked) {
+                    result += "X ";
+                } else {
+                    result += "  ";
+                }
+            }
+            result += "\n";
+        }
+        return result;
+    }
+
+    public bool MarkCell(int inNumber) {
+        for(int row = 0; row < 5; row++) {
+            for(int col = 0; col < 5; col++) {
+                if(Board[row,col].Number == inNumber) {
+                    Board[row,col].IsMarked = true;
+                    // TODO: Invoke method to evaluate and update IsBingo status;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public BingoBoard(List<string> input) {
+        int row = 0, col = 0;
+        foreach(string line in input) {
+            string[] terms = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach(string term in terms) {
+                BingoCell cell = new BingoCell(int.Parse(term));
+                Board[row,col] = cell;
+                col++;
+            }
+            col = 0;
+            row++;
+        }
+        Board[2,2].IsMarked = true;
+        IsBingo = false;
+    }
 
     public class BingoCell {
         public int Number { get; }
-        public bool Marked { get; set; }
+        public bool IsMarked { get; set; }
 
-        public BingoCell() : this(1)
-        {
-            Console.WriteLine(Number);
-        }
+        // public BingoCell() : this(1) { }
+
         public BingoCell(int inNumber) {
             Number = inNumber;
-            Marked = false;
+            IsMarked = false;
         }
     }
 }
