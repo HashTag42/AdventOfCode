bcc055d16ac02c26023ca119e4ba8a67eafcfac1
HashTag42 <ceegee@outlook.com>
2021-12-04 20:35:16 -0800

    Adding skeleton for 2021-05.

---
+++ 2021/2021-05/2021-05.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2021_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
