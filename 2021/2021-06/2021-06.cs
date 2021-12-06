faccc68699ea2b5cbc8f22ba4a31858e3c826d68
Cesar Garcia <cgarcia@bungie.com>
2021-12-06 00:29:47 -0800

    Solved 2021-06, parts 1 & 2.

--- 2021/2021-06/2021-06.cs
+++ 2021/2021-06/2021-06.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace _2021_06
 {
@@ -8,33 +9,41 @@ namespace _2021_06
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine(solvePuzzle(@".\testInput.txt", 80)); // Expect: 5934
-            // Console.WriteLine(solvePuzzle(@".\Input.txt", 80)); // Expect: 379414
-            Console.WriteLine(solvePuzzle(@".\testInput.txt", 256)); // Expect: 26984457539
-            // Console.WriteLine(solvePuzzle(@".\Input.txt", 256)); // Expect: ?
+            Console.WriteLine(SolvePuzzle(@".\testInput.txt", 80)); // Expect: 5934
+            Console.WriteLine(SolvePuzzle(@".\Input.txt", 80)); // Expect: 379414
+            Console.WriteLine(SolvePuzzle(@".\testInput.txt", 256)); // Expect: 26984457539
+            Console.WriteLine(SolvePuzzle(@".\Input.txt", 256)); // Expect: 1705008653296
         }
 
-        static int solvePuzzle(string FilePath, int Days) {
-            // Read input list of lanternfish
-            List<Lanternfish> group = new List<Lanternfish>();
+        static ulong SolvePuzzle(string FilePath, int Days) {
             string line = File.ReadAllText(FilePath);
-            string[] units = line.Split(',');
-            foreach(string u in units) {
-                group.Add(new Lanternfish(int.Parse(u)));
+            string[] input = line.Split(',');
+            ulong[] numbers = new ulong[10];
+            foreach(string u in input) {
+                numbers[int.Parse(u)]++;
             }
-            for(int day = 0; day < Days; day++) {
-                Console.WriteLine("Day {0}", day);
-                int newFish = 0;
-                foreach(Lanternfish f in group) {
-                    if(f.NextDay()) {
-                        newFish++;
-                    }
-                }
-                for(int i = 0; i < newFish; i++) {
-                    group.Add(new Lanternfish());
-                }
+            for(int d = 0; d < Days; d++) {
+                numbers = MoveNumbers(numbers);
             }
-            return group.Count;
+            return CountNumbers(numbers);
+        }
+
+        static ulong[] MoveNumbers(ulong[] Numbers) {
+            ulong[] newNumbers = new ulong[9];
+            newNumbers[6] = Numbers[0];
+            newNumbers[8] = Numbers[0];
+            for(int i = 0; i <= 7; i++) {
+                newNumbers[i] += Numbers[i+1];
+            }
+            return newNumbers;
+        }
+
+        static ulong CountNumbers(ulong[] Numbers) {
+            ulong count = 0;
+            for(int i = 0; i < 9; i++) {
+                count += Numbers[i];
+            }
+            return count;
         }
     }
 }
