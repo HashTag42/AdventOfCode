0690bd188081489cd3d56ecb8abc7b3a3f115974
Cesar Garcia <cgarcia@bungie.com>
2021-12-05 16:36:04 -0800

    Added skeleton for 2015-05.

---
+++ 2015/2015-05/2015-05.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2015_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
