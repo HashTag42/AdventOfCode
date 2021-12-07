a89bae37172c6a119cdc6333da0c3f4b9bd510cf
HashTag42 <ceegee@outlook.com>
2021-12-06 23:47:13 -0800

    Solved 2021-07 part 2.

--- 2021/2021-07/2021-07.cs
+++ 2021/2021-07/2021-07.cs
@@ -8,11 +8,14 @@ namespace _2021_07
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(solvePart1(@".\inputTest.txt")); //Expect 37
-            Console.WriteLine(solvePart1(@".\input.txt")); //Expect 323647
+            bool extra = true;
+            Console.WriteLine(solvePuzzle(@".\inputTest.txt")); //Expect 37
+            Console.WriteLine(solvePuzzle(@".\input.txt")); //Expect 323647
+            Console.WriteLine(solvePuzzle(@".\inputTest.txt", extra)); //Expect 168
+            Console.WriteLine(solvePuzzle(@".\input.txt", extra)); //Expect 87640209
         }
 
-        static int solvePart1(string FilePath) {
+        static int solvePuzzle(string FilePath, bool extra = false) {
             int arraySize= 2000;
             string line= File.ReadAllText(FilePath);
             string[] terms= line.Split(',');
@@ -22,21 +25,33 @@ namespace _2021_07
                 numbers[value]++;
             }
             int minimumFuel= int.MaxValue;
-            for(int position= 0; position < arraySize; position++) {
-                if(numbers[position] > 0) {
-                    int fuel= 0;
-                    for(int i= 0; i < arraySize; i++) {
-                        if(numbers[i] > 0) {
-                            int stepFuel= numbers[i] * Math.Abs(position-i);
-                            fuel+= stepFuel;
-                        }
+            for(int target= 0; target < arraySize; target++) {
+                int fuel= 0;
+                for(int source= 0; source < arraySize; source++) {
+                    if(numbers[source] > 0) {
+                        int stepFuel= CalculateFuel(numbers[source], source, target, extra);
+                        fuel+= stepFuel;
                     }
-                    if(fuel < minimumFuel) {
-                        minimumFuel= fuel;
-                    }
+                }
+                if(fuel < minimumFuel) {
+                    minimumFuel= fuel;
                 }
             }
             return minimumFuel;
+        }
+
+        static int CalculateFuel(int Count, int From, int To, bool extra) {
+            int fuelCost= 0;
+            int distance= Math.Abs(From - To);
+            if(extra) {
+                for(int i= 0; i <= distance; i++) {
+                    fuelCost+= i;
+                }
+                fuelCost= Count * fuelCost;
+            } else {
+                fuelCost+= Count * distance;
+            }
+            return fuelCost;
         }
     }
 }
