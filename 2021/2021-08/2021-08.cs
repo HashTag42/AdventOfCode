19ad86b1803e11790e0479b6bd2a5866dc3ccdfc
Cesar Garcia <cgarcia@bungie.com>
2021-12-07 16:44:08 -0800

    Getting ready for 2021-08

---
+++ 2021/2021-08/2021-08.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2021_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello 2021-08!");
+        }
+    }
+}
