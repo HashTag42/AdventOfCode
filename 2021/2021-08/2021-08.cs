df24c00e96eb8b25f6921198636ba983725df8f0
Cesar Garcia <cgarcia@bungie.com>
2021-12-07 22:39:34 -0800

    Solved 2021-08 part 1.

--- 2021/2021-08/2021-08.cs
+++ 2021/2021-08/2021-08.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+// Puzzle: https://adventofcode.com/2021/day/8
 
 namespace _2021_08
 {
@@ -6,7 +11,23 @@ namespace _2021_08
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello 2021-08!");
+            Console.WriteLine(solvePuzzle(@".\input.txt")); // Answer: 521.
+        }
+
+        static int solvePuzzle(string FilePath) {
+            int countEasyDigits = 0;
+            List<int> filter = new List<int> { 2, 4, 3, 7 };
+            List<string> lines = new List<string>(File.ReadAllLines(FilePath));
+            foreach(string line in lines) {
+                string[] segments = line.Split('|');
+                string[] digits = segments[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for(int j = 0; j < digits.Length; j++) {
+                    if(filter.IndexOf(digits[j].Length) != -1) {
+                        countEasyDigits++;
+                    }
+                }
+            }
+            return countEasyDigits;
         }
     }
 }
