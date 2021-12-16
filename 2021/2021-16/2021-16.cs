8fec1c0afb11bed65c132d00b97603e0efcd1a0e
HashTag42 <ceegee@outlook.com>
2021-12-16 00:14:00 -0800

    Refactoring for .NET 6 & adding samples.

--- 2021/2021-16/2021-16.cs
+++ 2021/2021-16/2021-16.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+// --- Day 16: Packet Decoder ---
+// https://adventofcode.com/2021/day/16
+
+using MyNamespace;
+
 Console.WriteLine("Hello, 2021-16!");
+
+int hex = 0x2F;
+int binary = 0b_0010_1111;
+
+Console.WriteLine(hex);
+Console.WriteLine(binary);
+
+Console.WriteLine(Utilities.ConvertFromBinary("100"));
+
+
+class Packet
+{
+    class Header
+    {
+        private enum type {literalType, operatorType};
+
+        class Version
+        {
+
+        }
+
+        class TypeID
+        {
+
+
+        }
+    }
+}
---
+++ 2021/2021-16/utilities.cs
@@ -0,0 +1,14 @@
+namespace MyNamespace
+{
+    public static class Utilities
+    {
+        public static int ConvertFromBinary(string s)
+        {
+            int result = 0;
+
+            return result;
+        }
+
+    }
+
+}
