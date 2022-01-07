b307b0c8b268c9f8d88f52d7671ccd2129dcafb4
HashTag42 <ceegee@outlook.com>
2022-01-06 23:34:48 -0800

    Solved 2015-12 Part 1.

---
+++ 2015/2015-12/2015-12.cs
@@ -0,0 +1,37 @@
+﻿/* --- Day 12: JSAbacusFramework.io --- https://adventofcode.com/2015/day/12
+
+Santa's Accounting-Elves need help balancing the books after a recent order. Unfortunately, their accounting software
+uses a peculiar storage format. That's where you come in.
+
+They have a JSON document which contains a variety of things: arrays ([1,2,3]), objects ({"a":1, "b":2}), numbers,
+and strings. Your first job is to simply find all of the numbers throughout the document and add them together.
+
+For example:
+
+[1,2,3] and {"a":2,"b":4} both have a sum of 6.
+[[[3]]] and {"a":{"b":4},"c":-1} both have a sum of 3.
+{"a":[-1,1]} and [-1,{"a":1}] both have a sum of 0.
+[] and {} both have a sum of 0.
+You will not encounter any strings containing numbers.
+
+What is the sum of all numbers in the document?
+
+*/
+
+using System.Text.RegularExpressions;   // Regex Class
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+string text = File.ReadAllText(@".\input.txt");
+
+/// PART 1 ///
+string pattern = "-?\\d+";
+Regex rg = new Regex(pattern);
+MatchCollection matches = rg.Matches(text);
+
+int sum = 0;
+for (int i = 0; i < matches.Count; i++)
+{
+    sum += int.Parse(matches[i].ToString());
+}
+Console.WriteLine(sum); // Expected: 191164
--- README.md
+++ README.md
@@ -93,6 +93,12 @@ Solutions to the [Advent of Code](https://adventofcode.com/) puzzles
 
 ## [2015](https://adventofcode.com/2015)
 
+### 2015 Day 12: [JSAbacusFramework.io](https://adventofcode.com/2015/day/12)
+
+* [My Solution](https://github.com/HashTag42/AdventOfCode/tree/main/2015/2015-12)
+* Part 1: Used the [RegEx](https://docs.microsoft.com/en-us/dotnet/api/system.text.regularexpressions.regex) Class to find all digits in the input.
+* Part 2:
+
 ### 2015 Day 11: [Corporate Policy](https://adventofcode.com/2015/day/11)
 
 * [My Solution](https://github.com/HashTag42/AdventOfCode/tree/main/2015/2015-11)
