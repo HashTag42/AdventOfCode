e20823992d436e440caed874a9c5bdc603abb4f6
César García <ceegee@outlook.com>
2025-12-03 13:51:56 -0800

    Refactor test cases

--- 2025/02/day_02.py
+++ 2025/02/day_02.py
@@ -29,3 +29,50 @@ def is_valid_ID_part1(id_num: int) -> bool:
 def is_valid_ID_part2(id_num: int) -> bool:
     id_str = str(id_num)
     return not re.match(r'^(.+?)\1+$', id_str)
+
+
+sum_invalid_IDs_test_cases = [
+    # file, expected_part1, expected_part2
+    ('./2025/02/example.txt', 1227775554, 4174379265),
+    ('./2025/02/input.txt', 38158151648, 45283684555),
+]
+
+is_valid_ID_test_cases = [
+    # id_int, expected_part1, expected_part2
+    (11, False, False),
+    (12, True, True),
+    (22, False, False),
+    (99, False, False),
+    (111, True, False),
+    (999, True, False),
+    (1010, False, False),
+    (1234, True, True),
+    (222222, False, False),
+    (446443, True, True),
+    (446446, False, False),
+    (446449, True, True),
+    (565653, True, True),
+    (565656, True, False),
+    (565659, True, True),
+    (1698522, True, True),
+    (1698528, True, True),
+    (38593856, True, True),
+    (38593859, False, False),
+    (38593862, True, True),
+    (824824821, True, True),
+    (824824824, True, False),
+    (824824827, True, True),
+    (1188511885, False, False),
+    (2121212118, True, True),
+    (2121212121, True, False),
+    (2121212124, True, True),
+]
+
+if __name__ == "__main__":
+    for test in sum_invalid_IDs_test_cases:
+        part1, part2 = sum_invalid_IDs(test[0])
+        assert part1 == test[1], f"sum_invalid_IDs: part1: {part1} is not equal to {test[1]}"
+        assert part2 == test[2], f"sum_invalid_IDs: part2: {part2} is not equal to {test[2]}"
+    for test in is_valid_ID_test_cases:
+        assert is_valid_ID_part1(test[0]) == test[1], f"is_valid_ID_part1({test[0]}) is not equal to {test[1]}"
+        assert is_valid_ID_part2(test[0]) == test[2], f"is_valid_ID_part2({test[0]}) is not equal to {test[2]}"
--- 2025/02/day_02_test.py
+++ 2025/02/day_02_test.py
@@ -6,90 +6,53 @@ from day_02 import (
 import pytest
 
 
-test_cases = [
+sum_invalid_IDs_test_cases = [
     # file, expected_part1, expected_part2
     ('./2025/02/example.txt', 1227775554, 4174379265),
     ('./2025/02/input.txt', 38158151648, 45283684555),
 ]
 
 
-@pytest.mark.parametrize("file, expected_part1, expected_part2", test_cases)
+@pytest.mark.parametrize("file, expected_part1, expected_part2", sum_invalid_IDs_test_cases)
 def test_sum_invalid_IDs(file, expected_part1, expected_part2):
     part1, part2 = sum_invalid_IDs(file)
     assert part1 == expected_part1
     assert part2 == expected_part2
 
 
-is_valid_ID_part1_test_cases = [
-    # id_int, expected
-
-    # invalid IDs
-    (11, False),
-    (22, False),
-    (99, False),
-    (1010, False),
-    (1188511885, False),
-    (222222, False),
-    (446446, False),
-    (38593859, False),
-
-    # valid IDs
-    (1698522, True),
-    (1698528, True),
-    (446443, True),
-    (446449, True),
-    (38593856, True),
-    (38593862, True),
-    (565653, True),
-    (565659, True),
-    (824824821, True),
-    (824824827, True),
-    (2121212118, True),
-    (2121212124, True),
+is_valid_ID_test_cases = [
+    # id_int, expected_part1, expected_part2
+    (11, False, False),
+    (12, True, True),
+    (22, False, False),
+    (99, False, False),
+    (111, True, False),
+    (999, True, False),
+    (1010, False, False),
+    (1234, True, True),
+    (222222, False, False),
+    (446443, True, True),
+    (446446, False, False),
+    (446449, True, True),
+    (565653, True, True),
+    (565656, True, False),
+    (565659, True, True),
+    (1698522, True, True),
+    (1698528, True, True),
+    (38593856, True, True),
+    (38593859, False, False),
+    (38593862, True, True),
+    (824824821, True, True),
+    (824824824, True, False),
+    (824824827, True, True),
+    (1188511885, False, False),
+    (2121212118, True, True),
+    (2121212121, True, False),
+    (2121212124, True, True),
 ]
 
 
-@pytest.mark.parametrize("id_int, expected", is_valid_ID_part1_test_cases)
-def test_is_valid_ID_part1(id_int, expected):
-    assert is_valid_ID_part1(id_int) == expected
-
-
-is_valid_ID_part2_test_cases = [
-    # id_int, expected
-
-    # invalid IDs
-    (11, False),
-    (22, False),
-    (99, False),
-    (111, False),
-    (999, False),
-    (1010, False),
-    (1188511885, False),
-    (222222, False),
-    (446446, False),
-    (38593859, False),
-    (565656, False),
-    (824824824, False),
-    (2121212121, False),
-
-    # valid IDs
-    (12, True),
-    (1234, True),
-    (1698522, True),
-    (1698528, True),
-    (446443, True),
-    (446449, True),
-    (38593856, True),
-    (38593862, True),
-    (565653, True),
-    (565659, True),
-    (824824821, True),
-    (824824827, True),
-    (2121212118, True),
-    (2121212124, True),
-]
-
-
-@pytest.mark.parametrize("id_int, expected", is_valid_ID_part2_test_cases)
-def test_is_valid_ID_part2(id_int, expected):
-    assert is_valid_ID_part2(id_int) == expected
+@pytest.mark.parametrize("id_int, expected_part1, expected_part2", is_valid_ID_test_cases)
+def test_is_valid_ID(id_int, expected_part1, expected_part2):
+    assert is_valid_ID_part1(id_int) == expected_part1
+    assert is_valid_ID_part2(id_int) == expected_part2
