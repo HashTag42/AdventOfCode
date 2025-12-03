3ed704845a948ba7abfa3d18703fb8a79cb546f5
César García <ceegee@outlook.com>
2025-12-02 16:18:18 -0800

    Refactor .rotate() to also return the number of clicks

--- 2025/01/Dial.py
+++ 2025/01/Dial.py
@@ -8,30 +8,59 @@ LENGTH = MAX - MIN + 1
 
 
 class Dial():
+    ################################################################################
+    # region CONSTRUCTOR
     def __init__(self, start: int = START) -> None:
         self.position: int = start
+    # endregion
+    ################################################################################
 
-    def rotate(self, rotation: str) -> int:
-        """Rotate the dial according to the rotation instruction and return the updated position."""
+    ################################################################################
+    # region PUBLIC INTERFACE
+    def rotate(self, rotation: str) -> tuple[int, int]:
+        """
+        Rotate the dial according to the rotation instruction
+        Returns the updated position and number of 'clicks' (zero hits)
+        """
 
-        # Break down the rotation instruction into direction and distance
+        direction, distance = self._parse_rotation(rotation)
+
+        quotient, remainder = divmod(distance, LENGTH)
+        clicks = quotient
+
+        # Rotate the dial
+        self.position += direction * remainder
+
+        #  Correct the position if gone past MAX or below MIN
+        if self.position < MIN:
+            self.position += LENGTH
+            clicks += 1
+        elif self.position > MAX:
+            self.position -= LENGTH
+            clicks += 1
+
+        return self.position, clicks
+    # endregion
+    ################################################################################
+
+    ################################################################################
+    # region PRIVATE INTERFACE
+    def _parse_rotation(self, rotation: str) -> tuple[int, int]:
+        """Parse the rotation instruction and return direction and distance"""
         match = re.match(r'([A-Z])(\d+)', rotation)
         if match:
-            direction, distance = match.groups()
+            direction_str, distance = match.groups()
         else:
-            raise ValueError("Unrecognized rotation")
-        distance = int(distance)
+            raise ValueError("Unrecognized rotation instruction")
 
-        if direction == "R":
-            factor = 1
-        elif direction == "L":
-            factor = -1
-        else:
-            raise ValueError("Unrecognized direction")
+        match(direction_str):
+            case "R":
+                direction_int = 1
+            case "L":
+                direction_int = -1
+            case _:
+                raise ValueError("Unrecognized direction")
 
-        # Rotate the dial. Correct the position if exceeded MAX
-        self.position += factor * distance % LENGTH
-        if self.position > MAX:
-            self.position -= LENGTH
-
-        return self.position
+        return direction_int, int(distance)
+    # endregion
+    ################################################################################
--- 2025/01/Dial_test.py
+++ 2025/01/Dial_test.py
@@ -1,47 +1,51 @@
-from Dial import Dial
+from Dial import Dial, START
 import pytest
 
 
 def test_Dial__init__():
     dial = Dial()
     assert isinstance(dial, Dial) is True
+    assert dial.position == START
 
 
 rotate_test_cases = [
-    # rotation, expected
-    ("L10", 40),
-    ("L50", 0),
-    ("L51", 99),
-    ("L52", 98),
-    ("L100", 50),
-    ("L210", 40),
-    ("R10", 60),
-    ("R49", 99),
-    ("R50", 0),
-    ("R51", 1),
-    ("R100", 50),
-    ("R210", 60),
+    # rotation, expected_position, expected_clicks
+    ("L10", 40, 0),
+    ("L50", 0, 0),
+    ("L51", 99, 1),
+    ("L52", 98, 1),
+    ("L100", 50, 1),
+    ("L210", 40, 2),
+    ("R10", 60, 0),
+    ("R49", 99, 0),
+    ("R50", 0, 1),
+    ("R51", 1, 1),
+    ("R100", 50, 1),
+    ("R210", 60, 2),
+    ("R1000", 50, 10),
 ]
 
 
-@pytest.mark.parametrize("rotation, expected", rotate_test_cases)
-def test_Dial_rotate(rotation, expected):
+@pytest.mark.parametrize("rotation, expected_position, expected_clicks", rotate_test_cases)
+def test_Dial_rotate(rotation: str, expected_position: int, expected_clicks: int):
     dial = Dial()
-    assert dial.rotate(rotation) == expected
+    position, clicks = dial.rotate(rotation)
+    assert position == expected_position
+    assert clicks == expected_clicks
 
 
 rotate_raises_test_cases = [
     # rotation
-    ("1"),
-    ("A"),
-    ("A5"),
-    ("5B"),
-    ("LR5"),
+    ("A"),      # Missing distance
+    ("1"),      # Missing direction
+    ("5B"),     # Missing direction
+    ("A5"),     # Incorrect direction
+    ("LR5"),    # Incorrect direction
 ]
 
 
 @pytest.mark.parametrize("rotation", rotate_raises_test_cases)
-def test_Dial_rotate_raises(rotation):
+def test_Dial_rotate_raises(rotation: str):
     dial = Dial()
     with pytest.raises(ValueError):
         dial.rotate(rotation)
