f3b6e180a314b419ee9b01dc71dfd32d164cb43e
César García <ceegee@outlook.com>
2025-12-03 17:19:08 -0800

    Add solution to 2025 Day 3 Part II

--- 2025/03/day_03.py
+++ 2025/03/day_03.py
@@ -4,48 +4,51 @@ Puzzle: https://adventofcode.com/2025/day/3
 '''
 
 
-def get_file_total_ouput(file: str) -> int:
+def get_total_ouput(file: str) -> tuple[int, int]:
     with open(file, 'r') as f:
         banks = f.readlines()
-    part1 = 0
+    part1 = part2 = 0
     for bank in banks:
-        part1 += get_bank_max_joltage(bank)
-    return part1
+        bank = bank.strip()
+        part1 += get_bank_max_joltage(bank, 2)
+        part2 += get_bank_max_joltage(bank, 12)
+    return part1, part2
 
 
-def get_bank_max_joltage(bank: str) -> int:
-    max_joltage = 0
-    length = len(bank) - 1
-    for i in range(length):
-        for j in range(i + 1, length + 1):
-            joltage = int(bank[i] + bank[j])
-            if joltage > max_joltage:
-                max_joltage = joltage
-    return max_joltage
+def get_bank_max_joltage(bank: str, batteries: int) -> int:
+    result = []
+    start = 0
+    for i in range(batteries):
+        needed = batteries - i - 1
+        search = len(bank) - needed
+        digits = [(bank[pos], pos) for pos in range(start, search)]
+        max_digit, max_pos = max(digits, key=lambda x: x[0])
+        result.append(max_digit)
+        start = max_pos + 1
+    return int(''.join(result))
 
 
 if __name__ == "__main__":
 
     get_bank_max_joltage_test_cases = [
-        # bank, expected_max_joltage
-        ("987654321111111", 98),
-        ("811111111111119", 89),
-        ("234234234234278", 78),
-        ("818181911112111", 92),
+        # bank, expected_part1, expected_part2
+        ("987654321111111", 98, 987654321111),
+        ("811111111111119", 89, 811111111119),
+        ("234234234234278", 78, 434234234278),
+        ("818181911112111", 92, 888911112111),
+    ]
+
+    get_total_output_test_cases = [
+        # file, expected_part1, expected_part2
+        ('./2025/03/example.txt', 357, 3121910778619),
+        ('./2025/03/input.txt', 17346, 172981362045136),
     ]
 
     for test in get_bank_max_joltage_test_cases:
-        actual = get_bank_max_joltage(test[0])
-        expected = test[1]
-        assert actual == expected, f"{actual} != {expected}"
+        assert get_bank_max_joltage(test[0], 2) == test[1]
+        assert get_bank_max_joltage(test[0], 12) == test[2]
 
-    get_file_total_output_test_cases = [
-        # file, expected_part1
-        ('./2025/03/example.txt', 357),
-        # ('./2025/03/input.txt', 0),
-    ]
-
-    for test in get_file_total_output_test_cases:
-        actual = get_file_total_ouput(test[0])
-        expected_part1 = test[1]
-        assert actual == expected_part1, f"{actual} != {expected_part1}"
+    for test in get_total_output_test_cases:
+        part1, part2 = get_total_ouput(test[0])
+        assert part1 == test[1]
+        assert part2 == test[2]
--- 2025/03/day_03_test.py
+++ 2025/03/day_03_test.py
@@ -1,31 +1,30 @@
-from day_03 import get_bank_max_joltage, get_file_total_ouput
+from day_03 import get_total_ouput, get_bank_max_joltage
 import pytest
 
 get_bank_max_joltage_test_cases = [
-    # bank, expected_max_joltage
-    ("987654321111111", 98),
-    ("811111111111119", 89),
-    ("234234234234278", 78),
-    ("818181911112111", 92),
+    # bank, expected_part1, expected_part2
+    ("987654321111111", 98, 987654321111),
+    ("811111111111119", 89, 811111111119),
+    ("234234234234278", 78, 434234234278),
+    ("818181911112111", 92, 888911112111),
 ]
 
 
-@pytest.mark.parametrize("bank, expected_max_joltage", get_bank_max_joltage_test_cases)
-def test_get_bank_max_joltage(bank, expected_max_joltage):
-    actual = get_bank_max_joltage(bank)
-    expected = expected_max_joltage
-    assert actual == expected, f"{actual} != {expected}"
-
-
-get_file_total_output_test_cases = [
-    # file, expected_part1
-    ('./2025/03/example.txt', 357),
-    ('./2025/03/input.txt', 17346),
+get_total_output_test_cases = [
+    # file, expected_part1, expected_part2
+    ('./2025/03/example.txt', 357, 3121910778619),
+    ('./2025/03/input.txt', 17346, 172981362045136),
 ]
 
 
-@pytest.mark.parametrize("file, expected_part1", get_file_total_output_test_cases)
-def test_get_file_total_output(file, expected_part1):
-    actual = get_file_total_ouput(file)
-    expected = expected_part1
-    assert actual == expected, f"{actual} != {expected}"
+@pytest.mark.parametrize("bank, expected_part1, expected_part2", get_bank_max_joltage_test_cases)
+def test_get_bank_max_joltage(bank, expected_part1, expected_part2):
+    assert get_bank_max_joltage(bank, 2) == expected_part1
+    assert get_bank_max_joltage(bank, 12) == expected_part2
+
+
+@pytest.mark.parametrize("file, expected_part1, expected_part2", get_total_output_test_cases)
+def test_get_total_output(file, expected_part1, expected_part2):
+    part1, part2 = get_total_ouput(file)
+    assert part1 == expected_part1
+    assert part2 == expected_part2
