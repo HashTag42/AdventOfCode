2e50526be944e91894f1960a3104196e97438b94
César García <ceegee@outlook.com>
2025-12-05 15:29:05 -0800

    Update template

--- 2025/template/day_2025_DAY.py
+++ 2025/template/day_2025_DAY.py
@@ -5,18 +5,23 @@ Puzzle: https://adventofcode.com/2025/day/DAY
 
 
 def solve_2025_DAY(file: str) -> tuple[int, int]:
-    with open(file, 'r') as f:
-        data = f.readlines()
-    part1, part2 = solve_part1(), solve_part2()
+    data = get_data(file)
+    part1, part2 = solve_part1(data), solve_part2(data)
     return part1, part2
 
 
-def solve_part1() -> int:
+def solve_part1(data) -> int:
     return 0
 
 
-def solve_part2() -> int:
+def solve_part2(data) -> int:
     return 0
+
+
+def get_data(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    return lines
 
 
 if __name__ == "__main__":
