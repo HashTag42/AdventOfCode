4797067caac6605e1607afb6a2153beadaf41f75
César García <ceegee@outlook.com>
2025-12-06 19:05:44 -0800

    Refactor tests to call get_data() only once

--- 2025/template/day_2025_DAY_test.py
+++ 2025/template/day_2025_DAY_test.py
@@ -4,15 +4,27 @@ import pytest
 test_cases = [
     # file, expected
     ('./2025/DAY/example.txt', [0, 0]),
-    ('./2025/DAY/input.txt', [0, 0]),
+    # ('./2025/DAY/input.txt', [0, 0]),
 ]
 
 
-@pytest.mark.parametrize("file, expected", test_cases)
-def test_solve_part1(file, expected):
-    assert solve_part1(get_data(file)[0]) == expected[0]
+@pytest.fixture
+def test_data(request):
+    """Fixture that loads data based on parametrized file"""
+    file = request.param[0]
+    expected = request.param[1]
+    data = get_data(file)
+    return data, expected
 
 
-@pytest.mark.parametrize("file, expected", test_cases)
-def test_solve_part2(file, expected):
-    assert solve_part2(get_data(file)[1]) == expected[1]
+@pytest.mark.parametrize("test_data", test_cases, indirect=True)
+def test_solve_part1(test_data):
+    data, expected = test_data
+    assert solve_part1(data[0]) == expected[0]
+
+
+@pytest.mark.parametrize("test_data", test_cases, indirect=True)
+def test_solve_part2(test_data):
+    data, expected = test_data
+    assert solve_part2(data[1]) == expected[1]
+
