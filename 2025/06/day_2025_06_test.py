a249854f5df2e3d59678cb8555e885ae0d73fb9d
César García <ceegee@outlook.com>
2025-12-06 19:02:44 -0800

    Refactor tests to invoke get_data() only once

--- 2025/06/day_2025_06_test.py
+++ 2025/06/day_2025_06_test.py
@@ -9,11 +9,22 @@ test_cases = [
 ]
 
 
-@pytest.mark.parametrize("file, expected", test_cases)
-def test_solve_part1(file, expected):
-    assert solve_part1(get_data(file)[0]) == expected[0]
+@pytest.fixture
+def test_data(request):
+    """Fixture that loads data based on parametrized file"""
+    file = request.param[0]
+    expected = request.param[1]
+    data = get_data(file)
+    return data, expected
 
 
-@pytest.mark.parametrize("file, expected", test_cases)
-def test_solve_part2(file, expected):
-    assert solve_part2(get_data(file)[1]) == expected[1]
+@pytest.mark.parametrize("test_data", test_cases, indirect=True)
+def test_solve_part1(test_data):
+    data, expected = test_data
+    assert solve_part1(data[0]) == expected[0]
+
+
+@pytest.mark.parametrize("test_data", test_cases, indirect=True)
+def test_solve_part2(test_data):
+    data, expected = test_data
+    assert solve_part2(data[1]) == expected[1]
