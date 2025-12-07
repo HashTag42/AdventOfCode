bd82468f5a5ebcadd2408b5549acad0318e7a98b
César García <ceegee@outlook.com>
2025-12-06 18:39:59 -0800

    Refactor tests to get results per file and per part

--- 2025/06/day_2025_06_test.py
+++ 2025/06/day_2025_06_test.py
@@ -1,21 +1,19 @@
-from day_2025_06 import solve_2025_06
+from day_2025_06 import solve_part1, solve_part2, get_data
 import pytest
 
 
 test_cases = [
-    # file, expected_part1, expected_part2
-    ('./2025/06/example.txt', 4277556, 3263827),
-    ('./2025/06/input.txt', 5667835681547, 9434900032651),
+    # file, expected
+    ('./2025/06/example.txt', [4277556, 3263827]),
+    ('./2025/06/input.txt', [5667835681547, 9434900032651]),
 ]
 
 
-@pytest.mark.parametrize("file, expected_part1, expected_part2", test_cases)
-def test_solve_2025_06_part1(file, expected_part1, expected_part2):
-    actual_part1, actual_part2 = solve_2025_06(file)
-    assert actual_part1 == expected_part1
+@pytest.mark.parametrize("file, expected", test_cases)
+def test_solve_part1(file, expected):
+    assert solve_part1(get_data(file)[0]) == expected[0]
 
 
-@pytest.mark.parametrize("file, expected_part1, expected_part2", test_cases)
-def test_solve_2025_06_part2(file, expected_part1, expected_part2):
-    actual_part1, actual_part2 = solve_2025_06(file)
-    assert actual_part2 == expected_part2
+@pytest.mark.parametrize("file, expected", test_cases)
+def test_solve_part2(file, expected):
+    assert solve_part2(get_data(file)[1]) == expected[1]
