dba1417025c152e104f144e0a29cab9e8b61b9c9
César García <ceegee@outlook.com>
2025-12-06 18:54:16 -0800

    Refactor template files

--- 2025/template/day_2025_DAY.py
+++ 2025/template/day_2025_DAY.py
@@ -5,23 +5,24 @@ Puzzle: https://adventofcode.com/2025/day/DAY
 
 
 def solve_2025_DAY(file: str) -> tuple[int, int]:
-    data = get_data(file)
-    part1, part2 = solve_part1(data), solve_part2(data)
-    return part1, part2
+    data1, data2 = get_data(file)
+    return solve_part1(data1), solve_part2(data2)
 
 
 def solve_part1(data) -> int:
-    return 0
+    result = 0
+    return result
 
 
 def solve_part2(data) -> int:
-    return 0
+    result = 0
+    return result
 
 
-def get_data(file):
+def get_data(file) -> tuple[list[str], list[str]]:
     with open(file, 'r') as f:
-        lines = f.readlines()
-    return lines
+        data1 = data2 = f.readlines()
+    return data1, data2
 
 
 if __name__ == "__main__":
--- 2025/template/day_2025_DAY_test.py
+++ 2025/template/day_2025_DAY_test.py
@@ -1,16 +1,18 @@
-from day_2025_DAY import solve_2025_DAY
+from day_2025_DAY import solve_part1, solve_part2, get_data
 import pytest
 
-
 test_cases = [
-    # file, expected_part1, expected_part2
-    ('./2025/DAY/example.txt', 0, 0),
-    ('./2025/DAY/input.txt', 0, 0),
+    # file, expected
+    ('./2025/DAY/example.txt', [0, 0]),
+    ('./2025/DAY/input.txt', [0, 0]),
 ]
 
 
-@pytest.mark.parametrize("file, expected_part1, expected_part2", test_cases)
-def test_solve_2025_DAY(file, expected_part1, expected_part2):
-    actual_part1, actual_part2 = solve_2025_DAY(file)
-    assert actual_part1 == expected_part1
-    assert actual_part2 == expected_part2
+@pytest.mark.parametrize("file, expected", test_cases)
+def test_solve_part1(file, expected):
+    assert solve_part1(get_data(file)[0]) == expected[0]
+
+
+@pytest.mark.parametrize("file, expected", test_cases)
+def test_solve_part2(file, expected):
+    assert solve_part2(get_data(file)[1]) == expected[1]
