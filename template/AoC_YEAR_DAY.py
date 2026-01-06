06405b87cce529a4cc2a33ba04b4d6ce79d30b01
César García <ceegee@outlook.com>
2026-01-05 18:53:43 -0800

    Refactor input_file consumption

--- template/AoC_YEAR_DAY.py
+++ template/AoC_YEAR_DAY.py
@@ -5,16 +5,16 @@ Puzzle: https://adventofcode.com/YEAR/day/DAY
 
 
 def solve(filename: str) -> tuple[int, int]:
-    data = get_data(filename)
-    return solve_part1(data), solve_part2(data)
+    lines = get_data(filename)
+    return solve_part1(lines), solve_part2(lines)
 
 
-def solve_part1(data) -> int:
+def solve_part1(lines: list[str]) -> int:
     result = 0
     return result
 
 
-def solve_part2(data) -> int:
+def solve_part2(lines: list[str]) -> int:
     result = 0
     return result
 
@@ -25,8 +25,10 @@ def get_data(filename: str) -> list[str]:
 
 
 if __name__ == "__main__":
-    result1, result2 = solve('./YEAR/FOLDER/example.txt')
-    print(f"example.txt: Results: Part 1 = {result1}, Part 2 = {result2}")
-
-    result1, result2 = solve('./YEAR/FOLDER/input.txt')
-    print(f"input.txt: Results: Part 1 = {result1}, Part 2 = {result2}")
+    input_files = [
+        './YEAR/FOLDER/example.txt',
+        './YEAR/FOLDER/input.txt',
+    ]
+    for input_file in input_files:
+        result1, result2 = solve(input_file)
+        print(f"{input_file=}: Results: {result1=}, {result2=}")
